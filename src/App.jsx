24c7b4344017385f1135ecda663b1dce16ffe7dc
artxmix <artem.artusevskij01@gmail.com>
2025-10-24 14:12:37 +0300

    ProtectedRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import HomePage from "./pages/HomePage/HomePage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import { Routes, Route } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import Layout from "./components/Layout/Layout";
+import ProtectedRoute from "./components/ProtectedRouter/ProtectedRouter";
 import "./styles/variables.css";
 import "./styles/global.css";
 
@@ -14,6 +16,17 @@ export default function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<Layout />}>
         <Route index element={<HomePage />} />
+      </Route>
+
+      <Route path="/profile" element={<Layout />}>
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
       </Route>
       <Route path="login" element={<LoginPage />} />
       <Route path="register" element={<RegisterPage />} />
---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./ProfilePage.module.css";
+
+export default function ProfilePage() {
+  return <p>Профиль</p>;
+}
