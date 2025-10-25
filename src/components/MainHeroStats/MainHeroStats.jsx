b6a596d1dbd483e465a6afca9a0ca8f8c9ef93ff
artxmix <artem.artusevskij01@gmail.com>
2025-10-25 15:51:41 +0300

    save

--- src/components/MainHeroStats/MainHeroStats.jsx
+++ src/components/MainHeroStats/MainHeroStats.jsx
@@ -3,8 +3,16 @@ import styles from "./MainHeroStats.module.css";
 import MainHeroStatItem from "../MainHeroStatItem/MainHeroStatItem";
 
 const stats = [
-  { id: 1, label: "Проблем решено", number: 13452 },
-  { id: 2, label: "Пользователей с нами", number: 34235 },
+  {
+    id: 1,
+    label: "Проблем решено",
+    number: Math.floor(Math.random() * 20000) + 1000,
+  },
+  {
+    id: 2,
+    label: "Пользователей с нами",
+    number: Math.floor(Math.random() * 50000) + 5000,
+  },
 ];
 
 export default function MainHeroStats() {
