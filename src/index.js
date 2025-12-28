10c82a17bd0f753c87d71127984d9dfb93ff1474
artxmix <artem.artusevskij01@gmail.com>
2025-12-29 00:13:38 +0300

    ScrollToTop

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./locales/i18n";
-import App from './App';
+import App from "./App";
+import ScrollToTop from "./features/route/ScrollToTop/ScrollToTop";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
