a31f090417749ef7e4d8d48359fa442f4443cb8e
artxmix <artem.artusevskij01@gmail.com>
2026-03-29 15:48:31 +0300

    save

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -1,23 +1,56 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./NavItem.module.css";
+import Dropdown from "../../common/Dropdown/Dropdown";
+import { PlayIcon } from "@heroicons/react/24/solid";
 
 export default function NavItem({ item, onClick }) {
   const Icon = item.icon;
+  const navigate = useNavigate();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  return (
-    <NavLink
-      to={item.to}
-      className={({ isActive }) =>
-        `${styles.navLink} ${isActive ? styles.activeLink : ""}`
-      }
-      onClick={() => {
-        if (onClick) onClick();
-      }}
-      aria-label={item.label}
-    >
-      {Icon && <Icon className={styles.icon} />}
-      <span className={styles.label}>{item.label}</span>
-    </NavLink>
-  );
+  if (item.type === "link") {
+    return (
+      <NavLink
+        to={item.to}
+        className={({ isActive }) =>
+          `${styles.navLink} ${isActive ? styles.activeLink : ""}`
+        }
+        onClick={() => {
+          if (onClick) onClick();
+        }}
+        aria-label={item.label}
+      >
+        {Icon && <Icon className={styles.icon} />}
+        <span className={styles.label}>{item.label}</span>
+      </NavLink>
+    );
+  }
+
+  if (item.type === "drop") {
+    const dropdownItems = item.items.map((subItem) => ({
+      label: subItem.label,
+      icon: subItem.icon,
+      onClick: () => {
+        navigate(subItem.to);
+      },
+    }));
+
+    return (
+      <div className={styles.dropNavItem}>
+        <button
+          className={styles.dropNavItemBtn}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+          <PlayIcon
+            className={`${styles.arrow} ${
+              isDropdownOpen ? styles.arrowRotated : ""
+            }`}
+          />
+          {item.label}
+        </button>
+        <Dropdown items={dropdownItems} visible={isDropdownOpen} />
+      </div>
+    );
+  }
 }
--- src/utils/useLinks.js
+++ src/utils/useLinks.js
@@ -82,23 +82,54 @@ export function useLinks() {
     ];
 
   return [
-    { id: 1, to: routes.home, label: t("nav.main"), icon: HomeIcon },
+    {
+      id: 1,
+      to: routes.home,
+      label: t("nav.main"),
+      icon: HomeIcon,
+      type: "link",
+    },
     {
       id: 2,
       to: routes.myRequests,
       label: t("nav.requests"),
       icon: ClipboardDocumentListIcon,
+      type: "link",
     },
-    { id: 3, to: routes.works, label: t("nav.works"), icon: Cog6ToothIcon },
-    { id: 4, to: routes.news, label: t("nav.news"), icon: MegaphoneIcon },
-    { id: 5, to: routes.statistic, label: t("nav.stat"), icon: ChartBarIcon },
-    { id: 6, to: routes.map, label: t("nav.map"), icon: MapIcon },
-    { id: 7, to: routes.faq, label: "FAQ", icon: InformationCircleIcon },
     {
-      id: 8,
-      to: routes.companies,
-      label: "Компании",
-      icon: ChatBubbleLeftRightIcon,
+      id: 3,
+      to: routes.works,
+      label: t("nav.works"),
+      icon: Cog6ToothIcon,
+      type: "link",
+    },
+    {
+      id: 4,
+      to: routes.news,
+      label: t("nav.news"),
+      icon: MegaphoneIcon,
+      type: "link",
+    },
+    {
+      id: 5,
+      label: "Ещё",
+      items: [
+        {
+          id: 5.1,
+          to: routes.statistic,
+          label: t("nav.stat"),
+          icon: ChartBarIcon,
+        },
+        { id: 5.2, to: routes.map, label: t("nav.map"), icon: MapIcon },
+        { id: 5.4, to: routes.faq, label: "FAQ", icon: InformationCircleIcon },
+        {
+          id: 5.5,
+          to: routes.companies,
+          label: "Компании",
+          icon: ChatBubbleLeftRightIcon,
+        },
+      ],
+      type: "drop",
     },
   ];
 }
